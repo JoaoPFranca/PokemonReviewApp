22d1c7cf91a7fdf777599621ae03d3d9a4415a6e
João Pedro de França Barboza <jpbarboza2004@gmail.com>
2024-05-08 12:10:19 -0300

    Entity Framework and Database Config.

--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -4,9 +4,8 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
         public string Text { get; set; }
-
+        public int Rating { get; set; }
         public Reviewer Reviewer { get; set; } //One-To-Many
         public Pokemon Pokemon { get; set; } //One-To-Many
     }
--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp;
+using PokemonReviewApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<Seed>(); //"Activating" the seed
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
