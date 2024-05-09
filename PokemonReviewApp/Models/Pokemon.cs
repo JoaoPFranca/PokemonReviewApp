e1f70d7b93348e8b2dc24ff3bd7bc18039a8b125
João Pedro de França Barboza <jpbarboza2004@gmail.com>
2024-05-09 13:30:04 -0300

    GET and READ methods (Pokemon Controller) part1

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -5,10 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
-
-
         public ICollection<Review> Reviews { get; set; } //One-To-Many
-
         public ICollection<PokemonOwner> PokemonOwners { get; set; } //Many-To-Many. Basically, I'm doing a One-To-Many with a join table.
         public ICollection<PokemonCategory> PokemonCategories { get; set; } //Many-To-Many. Basically, I'm doing a One-To-Many with a join table.
     }
--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<Seed>(); //"Activating" the seed
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DataContext>(options =>
